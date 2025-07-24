c2d1734333a68ffcab80a1c1fb32a90c48781c2e
Pc Silva <falecompcsilva@gmail.com>
2025-07-24 10:36:04 -0300

    Criando o ícone de remover despesas.

--- script.js
+++ script.js
@@ -76,8 +76,14 @@ function expenseAdd(newExpense){
         expenseAmount.classList.add("expense-amount")
         expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
 
+        // Cria o ícone de remover despesas.
+        const removeIcon = document.createElement("img")
+        removeIcon.classList.add("remove-icon")
+        removeIcon.setAttribute("src", "img/remove.svg")
+        removeIcon.setAttribute("alt", "remover")
+
         // Adiciona as informações no item.
-        expenseItem.append(expenseIcon, expenseInfo, expenseAmount)
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
 
         // Adiciona as informações a lista.
         expenseList.append(expenseItem)
